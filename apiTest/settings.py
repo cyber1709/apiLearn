324080c9d0f29e15670d0fa42ca1446ac9e18d24
cyber1709 <reachgauravarora@outlook.com>
2025-02-06 10:43:06 +0530

    swagger ui for api documentation added

--- apiTest/settings.py
+++ apiTest/settings.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework.authtoken', 
     'myapi',
-    'users'
+    'users',
+    'drf_yasg'
 ]
 
 MIDDLEWARE = [
--- myapi/urls.py
+++ myapi/urls.py
@@ -2,6 +2,27 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from myapi.views import UserProfileViewSet, UserLoginApiView, UserProfileFeedViewSet
 
+# imports for swagger 
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="My API",
+      default_version='v1',
+      description="Test description",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contact@myapi.local"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
+
+
+
 router = DefaultRouter()
 router.register('profile',  UserProfileViewSet)
 router.register('feed', UserProfileFeedViewSet )
@@ -10,5 +31,6 @@ router.register('feed', UserProfileFeedViewSet )
 
 urlpatterns = [ 
     path('login/', UserLoginApiView.as_view(), name='login'),
-    path('', include(router.urls))
+    path('', include(router.urls)),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
